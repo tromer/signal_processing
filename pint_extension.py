56def07cad81c1feabe6c30b43a66750daec5093
noam gavish <gavishnoam@gmail.com>
2014-08-27 23:20:36 +0300

    extensions to pint. histogram with units

---
+++ pint_extension.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 27 17:25:50 2014
+
+@author: noam
+"""
+
+import numpy as np
+import scipy as sp
+from scipy import signal
+import pint
+uerg = pint.UnitRegistry()
+
+def get_units(x):
+    if type(x.magnitude) == np.ndarray:
+        return 1.0 * x[0] / x[0].magnitude
+    else:
+        return 1.0 * x / x.magnitude
+#%%
+
+def magnitude_of_dimensionless(vec):
+    assert vec.dimensionless
+    return vec.magnitude
+    
+#%%
+    
+def test_magnitude_of_dimensionless():
+    raise NotImplementedError
+
+@uerg.wraps(None, (None, uerg.Hz, uerg.Hz, None, None, None, uerg.Hz))    
+def firwin_pint(numtaps, cutoff, width, window, pass_zero, scale, nyq):
+    return sp.signal.firwin(numtaps, cutoff, width, window, pass_zero, scale, nyq)
+#%%
+    
+def histogram(a, bins=10, range_=None, weights=None, density=None):
+    base_units = get_units(a)
+    a = a.magnitude
+    if not type(bins) == int:
+        bins = bins.to(base_units)
+        bins = bins.magnitude
+    if range_:
+        range_ = range_.to(base_units)
+        range_ = range_.magnitude
+   
+    hist, edges = np.histogram(a, bins, range_, weights, density)
+    return hist, edges * base_units
