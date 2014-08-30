1680f2d8e45f96e1ab624bb2b10b1aad354b1d15
noam gavish <gavishnoam@gmail.com>
2014-08-30 17:19:03 +0300

    allclose, histogram with units works well

--- pint_extension.py
+++ pint_extension.py
@@ -8,42 +8,103 @@ Created on Wed Aug 27 17:25:50 2014
 import numpy as np
 import scipy as sp
 from scipy import signal
-
+import pint
+from pint import UnitRegistry
+uerg = UnitRegistry()
+#%%
 #from . import ureg, Q_
+
+def allclose(a, b, rtol=1e-5, atol=None):
+    a_ = a.to_base_units()
+    b_ = b.to_base_units()
+    
+    if not atol:
+        return np.allclose(a_.magnitude, b_.magnitude, rtol)
+        
+    else:
+        atol_ = atol.to_base_units()
+        return np.allclose(a_.magnitude, b_.magnitude, rtol, atol_.magnitude)
+
+def test_allclose():
+    a = 3 * uerg.meter
+    b = 3 * uerg.meter
+    c = 3 * uerg.centimeter
+    d = 300 * uerg.centimeter
+    atol= 1 * uerg.centimeter
+    assert allclose(a, b)
+    assert allclose(a, d)
+    assert not allclose(a, c)
+    assert allclose(a, b, atol=atol)
+    #TODO: check that using a different unit for atol raises exception.
+    #TODO: add assertions. this is a very fondemental function.
+    
+test_allclose()
+#%%
 
 def get_units(x):
     if type(x.magnitude) == np.ndarray:
-        return 1.0 * x[0] / x[0].magnitude
+        x_ = x[np.nonzero(x)]
+        if len(x) == 0:
+            raise NotImplementedError
+        return 1.0 * x_[0] / x_[0].magnitude
     else:
+        if x.magnitude == 0:
+            raise NotImplementedError
         return 1.0 * x / x.magnitude
+        
+def test_get_units():
+    x = 3 * uerg.meter
+    assert allclose(get_units(x), 1 * uerg.meter)
+    vec = np.arange(1, 5) * uerg.meter
+    assert allclose(get_units(vec), uerg.meter)
+    
+test_get_units()
 #%%
 
-def magnitude_of_dimensionless(vec):
-    assert vec.dimensionless
-    return vec.magnitude
-    
-#%%
-    
-def test_magnitude_of_dimensionless():
-    raise NotImplementedError
-
-@uerg.wraps(None, (None, uerg.Hz, uerg.Hz, None, None, None, uerg.Hz))    
-def firwin_pint(numtaps, cutoff, width, window, pass_zero, scale, nyq):
-    return sp.signal.firwin(numtaps, cutoff, width, window, pass_zero, scale, nyq)
-#%%
     
 def histogram(a, bins=10, range_=None, weights=None, density=None):
-    if not type(a) == pint.unit.Quantity:
-        return np.histogram(a, bins, range_, weights, density)
+    """
+    histogram for vectors with quantities.
+    it's basically a wrap around np.histogram
+    """
+    # maybe should accept also Range object?
+    if not type(a) == uerg.Quantity:
+        raise NotImplementedError
+        #return np.histogram(a, bins, range_, weights, density)
     else:
         base_units = get_units(a)
         a = a.magnitude
         if not type(bins) == int:
             bins = bins.to(base_units)
             bins = bins.magnitude
-        if range_:
+        
+        if range_ != None:
             range_ = range_.to(base_units)
             range_ = range_.magnitude
-       
+            
         hist, edges = np.histogram(a, bins, range_, weights, density)
         return hist, edges * base_units
+
+        
+def test_histogram():
+    a = (np.arange(10) + 0.5) * uerg.meter
+    range_ = np.array([0, 10]) * uerg.meter
+    expected_hist = np.ones(10)
+    expected_edges = np.arange(11) * uerg.meter
+    hist, edges = histogram(a, bins=10, range_=range_)
+    assert np.allclose(hist, expected_hist)
+    assert allclose(edges, expected_edges)
+    
+test_histogram()
+
+#%%
+"""    
+    def freq_filter(contin, freq_ranges, ?, ?, ?):
+    raise NotImplementedError
+    
+   
+    @uerg.wraps(None, (None, uerg.Hz, uerg.Hz, None, None, None, uerg.Hz))    
+    def firwin_pint(numtaps, cutoff, width, window, pass_zero, scale, nyq):
+        return sp.signal.firwin(numtaps, cutoff, width, window, pass_zero, scale, nyq)
+        
+"""
