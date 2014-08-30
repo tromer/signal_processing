038c698bce1ab9214c4e12cde10b7f029331b7be
noam gavish <gavishnoam@gmail.com>
2014-08-30 17:20:21 +0300

    added some unimplemented funcs, and some remarks

--- continuous_data.py
+++ continuous_data.py
@@ -5,7 +5,10 @@ Created on Sat Aug 30 01:29:39 2014
 @author: noam
 """
 
-class ContinuousData(object)
+import numpy as np
+import pint_extension
+
+class ContinuousData(object):
     """
     this class represents any kind of continuous data (one dimensional).
     It includes a few kinds that first seem different from each other, has a lot in common.
@@ -30,6 +33,13 @@ class ContinuousData(object)
     1. we *do not* assume even sampling distance. that would be included in a subclass.
     2. this class is intentioned to be used with units. it's real world measurements.
     3. measurement errors are not handled here.
+    4. It may seem like a big overhead to use this object instead of just numpy.ndarray
+    this is not so. This object prevents errors of bad handling of sample rate.
+    It also works naturally with units, that prevents other errors.
+    For example: diff sould take the distance between samples into account.
+    FFT sould take the sample rate into account, and so on.
+    When you encounter an operation that is not implemented for ContinuousData,
+    the correct thing to do is to wrap the numpy or scipy operation.
     
     """
     def __init__(self, domain_samples, values):
@@ -51,25 +61,38 @@ class ContinuousData(object)
         is_each_in_range = self.domain_samples in domain_range
         return ContinuousData(self.domain_range[is_each_in_range], self.values[is_each_in_range])
         
+    def is_close(self, other, domain_rtol=None, domain_atol=None, values_rtol=None, values_atol=None):
+        return pint_extension.allclose(self.domain_samples, other.domain_samples, domain_rtol, domain_atol) \
+        and pint_extension.allclose(self.values, other.values, values_rtol, values_atol)
+        
+        
+        
     def DFT(self):
         raise NotImplementedError
         # maybe there is an issue regarding using DFT or IDTF, depending the domain
         # maybe it should be an extra param. seying which one to use
 
+  
+
 
 def plot(contin):
     # should be more  inputs
+    # maybe optional parameter of on which figure / axes to plot
     assert contin type?
     raise NotImplementedError
         # return fig, axes??
+def plot_quick(contin):
+    # creat the figure here
     
 class ContinuousDataEven(ContinuousData):
     """
+    read the ContinuousData documentation.
     the domain samples are evenly spaced
     """
-    def __init__(self, sample_step, values):
+    def __init__(self, values, sample_step, first_sample=0):
         # would there be a problem because of interface issues?
         self._sample_step = sample_step
+        self._first_sample = first_sample
         self._values = values
         
     @property
@@ -79,10 +102,14 @@ class ContinuousDataEven(ContinuousData):
     @property
     def sample_rate(self):
         return 1.0 / sample_step
+    
+    @property
+    def first_sample(self):
+        return self._first_sample
         
     @property
     def domain_samples(self):
-        return np.arange(len(self.values)) * self.sample_step
+        return np.arange(len(self.values)) * self.sample_step + first_sample
         
     def __getitem__(self, domain_range):
         bottom_index = np.ceil(domain_range.start / self.sample_step)
@@ -90,7 +117,20 @@ class ContinuousDataEven(ContinuousData):
         return ContinuousDataEven(self.sample_step, self.values[bottom_index:top_index])
         
     def FFT(self, ?):
+        raise NotImplementedError
         spectrum = 
         freq_step = 
         return ContinuousDataEven(freq_step, spectrum)
         
+def read_wav(filename, domain_unit=uerg.sec, value_unit=uerg.volt, expected_sample_rate=None, sample_rate_tolerance=None):
+    raise NotImplementedError
+    return signal
+    
+def freq_filter(contin, freq_ranges, ?, ?, ?):
+    raise NotImplementedError
+    
+    """   
+    @uerg.wraps(None, (None, uerg.Hz, uerg.Hz, None, None, None, uerg.Hz))    
+    def firwin_pint(numtaps, cutoff, width, window, pass_zero, scale, nyq):
+        return sp.signal.firwin(numtaps, cutoff, width, window, pass_zero, scale, nyq)
+    """
