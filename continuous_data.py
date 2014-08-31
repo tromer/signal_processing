46519442071f4b314a2dba9de8eb0b3e649b2724
noam gavish <gavishnoam@gmail.com>
2014-08-31 16:09:14 +0300

    implemented freq filter for signal. I tested it visually on white noise, and it works. but the test is not automatic now

--- continuous_data.py
+++ continuous_data.py
@@ -295,6 +295,37 @@ def freq_filter(contin, freq_ranges, ?, ?, ?):
         return sp.signal.firwin(numtaps, cutoff, width, window, pass_zero, scale, nyq)
 """
 
+def band_pass_filter(sig, freq_range, mask_len):
+    warnings.warn('not tested well')
+    #TODO: test well
+    freq_range.edges.ito(sig.sample_rate.units)
+    # if error rises with firwin with units, wrap it: http://pint.readthedocs.org/en/0.5.1/wrapping.html
+    mask_1 = sp.signal.firwin(mask_len, freq_range.edges.magnitude, pass_zero=False, nyq=0.5 * sig.sample_rate.magnitude)
+    filterred_values = np.convolve(sig.values.magnitude, mask_1, mode="same") * pint_extension.get_units(sig.values)
+    filterred = ContinuousDataEven(filterred_values, sig.sample_step, sig.first_sample)
+    return filterred
+    
+def test_band_pass_filter():
+    sample_step = uerg.sec
+    np.random.seed(13)
+    white_noise = ContinuousDataEven((np.random.rand(2048) - 0.5)* uerg.mamp, sample_step)
+    white_noise_spec = fft(white_noise)
+    freq_range = Range(np.array([0.3, 0.4]) * uerg.Hz)
+    white_noise_filterred = band_pass_filter(white_noise, freq_range, 32)
+    white_noise_filterred_spec = fft(white_noise_filterred)
+    plot_quick(white_noise_spec, is_abs=True)
+    plot_quick(white_noise_filterred_spec, is_abs=True)
+    
+    
+    
+    
+test_band_pass_filter()
+    
+    
+#%%
+
+
+
 def read_wav(filename, domain_unit=uerg.sec, first_sample=0, value_unit=uerg.milliamp, expected_sample_rate_and_tolerance=None):
     sample_rate, raw_sig = sp.io.wavfile.read(filename)
     sample_rate = 1.0 * sample_rate / domain_unit
