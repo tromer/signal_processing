61a7f3d807390720d63054f44c20cab59a148f98
noam gavish <gavishnoam@gmail.com>
2014-08-31 12:37:59 +0300

    smaller power of 2 changed to close power of 2, with mode input

--- numpy_extension.py
+++ numpy_extension.py
@@ -7,23 +7,35 @@ Created on Thu Aug 21 17:49:27 2014
 
 import numpy as np
 
-def smaller_power_of_2(n):
+def close_power_of_2(n, mode='smaller'):
     if type(n) == np.ndarray:
         assert (n >= 1).all()
     else:
         assert n >= 1
-    the_power = np.floor(np.log(n) / np.log(2))
+    
+    the_power = np.log(n) / np.log(2)
+    if mode == 'smaller':
+        the_power = np.floor(the_power)
+    elif mode == 'bigger':
+        the_power = np.ceil(the_power)
+    elif mode == 'closer':
+        the_power = np.round(the_power)
+    
     if type(n) == np.ndarray:
         return np.array(2 ** the_power, dtype=int)
     else:
         return int(2 ** the_power)
     
-def test_smaller_power_of_2():
+def test_close_power_of_2():
     print "testing"
-    inputs = np.array([1, 1.5, 2, 3, 4, 10, 20])
-    expeced_outputs = np.array([1, 1, 2, 2, 4, 8, 16])
-    assert np.allclose(smaller_power_of_2(inputs), expeced_outputs)
-    assert smaller_power_of_2(30) == 16
+    inputs = np.array([1, 1.6, 2, 3, 4, 10, 20])
+    expeced_outputs_smaller = np.array([1, 1, 2, 2, 4, 8, 16])
+    expeced_outputs_bigger = np.array([1, 2, 2, 4, 4, 16, 32])
+    expeced_outputs_closer = np.array([1, 2, 2, 4, 4, 8, 16])
+    assert np.allclose(close_power_of_2(inputs, 'smaller'), expeced_outputs_smaller)
+    assert np.allclose(close_power_of_2(inputs, 'bigger'), expeced_outputs_bigger)
+    assert np.allclose(close_power_of_2(inputs, 'closer'), expeced_outputs_closer)
+    assert close_power_of_2(30) == 16
     
 #%%
 def normalize(vec, ord=None, axis=None):
@@ -38,7 +50,7 @@ def test_normalize():
     
     
     
-test_smaller_power_of_2()
+test_close_power_of_2()
 test_normalize()
 
 #%%
