d32a09799e2f438de2d4ad1a78cbdbf7c1653106
noam gavish <gavishnoam@gmail.com>
2014-10-04 00:22:35 +0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='signal_processing',
+      version='0.1',
+      description='natural interface for signal processing of signals and pulses',
+      url='http://github.com/__________/_________',
+      author='Noam Gavish',
+      author_email='gavishnoam@gmail.com',
+      license='_______',
+      packages=['signal_processing'],
+      zip_safe=False)
