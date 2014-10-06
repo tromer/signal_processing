a5576af6a4d96a6a1640279fa3f2fe47aa8d5563
noam gavish <gavishnoam@gmail.com>
2014-10-06 17:30:45 +0300

    removed from X import *

--- signal_processing/__init__.py
+++ signal_processing/__init__.py
@@ -7,17 +7,17 @@ Q_ = uerg.Quantity
 
 from .extensions import numpy_extension, scipy_extension, pint_extension
 
-import segment
 from .segment import Segment
 import segments
-from .segments import Segments, SegmentsOfContinuous
+from .segments.segments_obj import Segments
+from .segments.segments_of_continuous_obj import SegmentsOfContinuous
 
 import continuous
 from .continuous.continuous_data_obj import ContinuousData
 from .continuous.continuous_data_even_obj import ContinuousDataEven
 
-from .histograms import *
-from .threshold import *
+import histograms
+import threshold
 
 # __all__ is not included here, since I do not expect from signal_processing import *, a statement that will create great confusion
 
