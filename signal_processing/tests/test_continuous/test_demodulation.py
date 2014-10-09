2a81d71196019263f38584faac1018f3dbbbdd19
noam gavish <gavishnoam@gmail.com>
2014-10-09 22:16:05 +0300

    commented out the test of am_conolution and am_filter

--- signal_processing/tests/test_continuous/test_demodulation.py
+++ signal_processing/tests/test_continuous/test_demodulation.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+from signal_processing.extensions.plt_extension import plot_few
+
 from signal_processing import uerg
 
-from signal_processing.extensions import pint_extension
 
 from signal_processing.continuous.continuous_data_even_obj import ContinuousDataEven
 
@@ -10,7 +11,6 @@ from signal_processing.segment import Segment
 
 from signal_processing.continuous import demodulation
 from signal_processing.continuous import generators
-from signal_processing.continuous.plots import plot, plot_quick
 
 def test_pm_demodulation():
     check_range = Segment(np.array([2, 30]) * uerg.ksec)
@@ -28,11 +28,9 @@ def test_pm_demodulation():
     sine = ContinuousDataEven(np.sin(phase) * uerg.mamp, sample_step)
     expected_phase_sig = ContinuousDataEven(phase, sample_step)
     phase_sig = demodulation.pm_demodulation(sine)
-    print phase_sig.first_sample, phase_sig.last_sample
-    print expected_phase_sig.first_sample, expected_phase_sig.last_sample
+    assert phase_sig[check_range].is_close(expected_phase_sig[check_range], values_rtol=0.01)
     # weird, it acctually gives phase diff of 0.5 pi from what I expect
-    assert pint_extension.allclose(phase_sig.first_sample, expected_phase_sig.first_sample)
-    assert pint_extension.allclose(phase_sig.last_sample, expected_phase_sig.last_sample, atol=min(phase_sig.sample_step, expected_phase_sig.sample_step))
+
     #fig, junk = plot_quick(expected_phase_sig)
     #plot(phase_sig, fig)
     #assert pint_extension.allclose(phase_sig.sample_step, expected_phase_sig.sample_step)
@@ -59,11 +57,8 @@ def test_am_demodulation_hilbert():
     sine = generators.generate_sine(sample_step, n_samples, amp, sine_freq=freq)
     expected_sine_am = ContinuousDataEven(np.ones(sine.n_samples) * amp, sample_step)
     sine_am = demodulation.am_demodulation_hilbert(sine)
-    """
-    plot_quick(sine)
-    plot_quick(sine_am)
-    plot_quick(expected_sine_am)
-    """
+    plot_few(sine, sine_am, expected_sine_am)
+
     assert sine_am[check_range].is_close(expected_sine_am[check_range], values_rtol=0.01)
     
     """
@@ -72,77 +67,60 @@ def test_am_demodulation_hilbert():
     sig = generate_square_freq_modulated(sample_step, n_samples, amp, freq, period)
     expected_sig_am = generate_square(sample_step, n_samples, amp, period)
     sig_am = am_demodulation_hilbert(sig)
-    fig, junk = plot_quick(sig)
-    plot(expected_sig_am, fig)
-    plot(sig_am, fig)
-    plot_quick(sig_am - expected_sig_am, fig)
-    # the big tolerance is due to gibs effect
+    plot_few(sig, expected_sig_am, sig_am, sig_am - expected_sig_am)
+   # the big tolerance is due to gibs effect
     assert sig_am[check_range].is_close_l_1(expected_sig_am[check_range], values_rtol=0.2, values_atol=0.2 * amp)
     """
  
-def XXX_test_am_demodulation_convolution():
-    check_range = Segment(np.array([2, 30]) * uerg.ksec)
-    sample_step = 1.0 * uerg.sec
-    n_samples = 2 ** 15
-    freq_1 = 0.15 * uerg.Hz
-    freq_2 = 0.40 * uerg.Hz
-    amp = uerg.mamp
-    sine_1 = generators.generate_sine(sample_step, n_samples, amp, sine_freq=freq_1)
-    sine_2 = generators.generate_sine(sample_step, n_samples, amp, freq_2)
-    sig = sine_1 + sine_2
-    """
-    #copied from test_am_demodulation_filter
-    dt = 1.0 / freq_1 * 0.25
-    am = am_demodulation_convolution(sig, dt)
-    fig, junk = plot(sig)
-    plot(sine_1, fig)
-    plot(am, fig)
-    plot_quick(sine_1 - am)
-    assert sine_1.is_close(am, domain_rtol=0.01, domain_atol=0.1 * uerg.mamp)
-    """
-    dt = 1.0 / freq_1 * 3
-    period = 100 * uerg.sec
-    sig = generators.generate_square_freq_modulated(sample_step, n_samples, amp, freq_1, period)
-    expected_sig_am = generators.generate_square(sample_step, n_samples, amp, period)
-    sig_am = generators.am_demodulation_convolution(sig, dt)
-    fig, junk = plot_quick(sig)
-    plot(expected_sig_am, fig)
-    plot(sig_am, fig)
-    plot_quick(sig_am - expected_sig_am, fig)
-    # the big tolerance is due to gibs effect
-    assert sig_am[check_range].is_close_l_1(expected_sig_am[check_range], values_rtol=0.2, values_atol=0.2 * amp)
+#def XXX_test_am_demodulation_convolution():
+    #check_range = Segment(np.array([2, 30]) * uerg.ksec)
+    #sample_step = 1.0 * uerg.sec
+    #n_samples = 2 ** 15
+    #freq_1 = 0.15 * uerg.Hz
+    #freq_2 = 0.40 * uerg.Hz
+    #amp = uerg.mamp
+    #sine_1 = generators.generate_sine(sample_step, n_samples, amp, sine_freq=freq_1)
+    #sine_2 = generators.generate_sine(sample_step, n_samples, amp, freq_2)
+    #sig = sine_1 + sine_2
+    #"""
+    ##copied from test_am_demodulation_filter
+    #dt = 1.0 / freq_1 * 0.25
+    #am = am_demodulation_convolution(sig, dt)
+    #assert sine_1.is_close(am, domain_rtol=0.01, domain_atol=0.1 * uerg.mamp)
+    #"""
+    #dt = 1.0 / freq_1 * 3
+    #period = 100 * uerg.sec
+    #sig = generators.generate_square_freq_modulated(sample_step, n_samples, amp, freq_1, period)
+    #expected_sig_am = generators.generate_square(sample_step, n_samples, amp, period)
+    #sig_am = demodulation.am_demodulation_convolution(sig, dt)
+    #plot_few(sig, expected_sig_am, sig_am, sig_am - expected_sig_am)
+   ## the big tolerance is due to gibs effect
+    #assert sig_am[check_range].is_close(expected_sig_am[check_range], values_rtol=0.2, values_atol=0.2 * amp)
 
-def XXX_test_am_demodulation_filter():
-    check_range = Segment(np.array([2, 30]) * uerg.ksec)
-    sample_step = 1.0 * uerg.sec
-    n_samples = 2 ** 15
-    freq_1 = 0.15 * uerg.Hz
-    freq_2 = 0.40 * uerg.Hz
-    amp = uerg.mamp
-    dt = 1.0 / freq_1 * 0.5    
-    """
-    sine_1 = generate_sine(sample_step, n_samples, amp, sine_freq=freq_1)
-    sine_2 = generate_sine(sample_step, n_samples, amp, freq_2)
-    sig = sine_1 + sine_2
+#def XXX_test_am_demodulation_filter():
+    #check_range = Segment(np.array([2, 30]) * uerg.ksec)
+    #sample_step = 1.0 * uerg.sec
+    #n_samples = 2 ** 15
+    #freq_1 = 0.15 * uerg.Hz
+    #freq_2 = 0.40 * uerg.Hz
+    #amp = uerg.mamp
+    #dt = 1.0 / freq_1 * 0.5    
+    #"""
+    #sine_1 = generate_sine(sample_step, n_samples, amp, sine_freq=freq_1)
+    #sine_2 = generate_sine(sample_step, n_samples, amp, freq_2)
+    #sig = sine_1 + sine_2 
     
     
-    am = am_demodulation_filter(sig, dt, 128)
-    fig, junk = plot(sig)
-    plot(sine_1.abs(), fig)
-    plot(am, fig)
-    plot_quick(sine_1.abs() - am)
-    assert sine_1.is_close(am, domain_rtol=0.01, domain_atol=0.1 * uerg.mamp)
-    """
+    #am = am_demodulation_filter(sig, dt, 128)
+    #assert sine_1.is_close(am, domain_rtol=0.01, domain_atol=0.1 * uerg.mamp)
+    #"""
     
-    dt = 1.0 / freq_1 * 3
-    period = 100 * uerg.sec
-    sig = generators.generate_square_freq_modulated(sample_step, n_samples, amp, freq_1, period)
-    expected_sig_am = generators.generate_square(sample_step, n_samples, amp, period)
-    sig_am = demodulation.am_demodulation_filter(sig, dt, 256)
-    fig, junk = plot_quick(sig)
-    plot(expected_sig_am, fig)
-    plot(sig_am, fig)
-    plot_quick(sig_am - expected_sig_am, fig)
-    # the big tolerance is due to gibs effect
-    assert sig_am[check_range].is_close_l_1(expected_sig_am[check_range], values_rtol=0.2, values_atol=0.2 * amp)
+    #dt = 1.0 / freq_1 * 3
+    #period = 100 * uerg.sec
+    #sig = generators.generate_square_freq_modulated(sample_step, n_samples, amp, freq_1, period)
+    #expected_sig_am = generators.generate_square(sample_step, n_samples, amp, period)
+    #sig_am = demodulation.am_demodulation_filter(sig, dt, 256)
+    #plot_few(sig, expected_sig_am, sig_am, sig_am - expected_sig_am)
+   ## the big tolerance is due to gibs effect
+    #assert sig_am[check_range].is_close(expected_sig_am[check_range], values_rtol=0.2, values_atol=0.2 * amp)
 
