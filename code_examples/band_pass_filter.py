c7784a8e724a5b0a1dada21ca2ebc9d78b7e2d67
noam gavish <gavishnoam@gmail.com>
2014-10-09 17:11:42 +0300

    changing an example

--- code_examples/band_pass_filter.py
+++ code_examples/band_pass_filter.py
@@ -3,12 +3,15 @@ from signal_processing import continuous as cont
 from signal_processing import uerg
 
 white_noise = cont.generators.white_noise(uerg.sec, n_samples=2**16, amplitude=uerg.mamp)
+white_noise.values_description = "white noise"
 
 freq_range = Segment([0.3, 0.4], uerg.Hz)
 white_noise_filterred = cont.filters.band_pass_filter(white_noise, freq_range, mask_len=32)
+white_noise_filterred.values_description = "filterred"
 
-white_noise_spec = cont.fft(white_noise)
-white_noise_filterred_spec = cont.fft(white_noise_filterred)
-fig = cont.plot(white_noise_spec, is_abs=True)[0]
-cont.plot(white_noise_filterred_spec, fig, is_abs=True)
+fig_time = white_noise.plot()
+white_noise_filterred.plot(fig_time)
+
+fig_spectrum = white_noise.fft().abs().plot()
+white_noise_filterred.fft().abs().plot(fig_spectrum)
 
