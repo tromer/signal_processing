5918c9ffe7d93e8db24a813d36e4798094aaba25
noam gavish <gavishnoam@gmail.com>
2014-10-10 02:09:53 +0300

    find_pulses example

--- code_examples/find_pulses.py
+++ code_examples/find_pulses.py
@@ -1,5 +1,6 @@
 from signal_processing import Segment
 from signal_processing import continuous as cont
+from signal_processing.extensions.plt_extension import mark_horizontal_lines
 from signal_processing import threshold
 from signal_processing import uerg
 
@@ -7,3 +8,5 @@ sig = cont.generators.square_freq_modulated(uerg.sec, 2**9, uerg.volt, sine_freq
 pulses = threshold.crosses(cont.demodulation.am_hilbert(sig), threshold=0.5 * uerg.volt)
 fig = sig.plot()
 pulses.mark_edges(fig)
+mark_horizontal_lines(0.5, fig, "thrshold")
+
