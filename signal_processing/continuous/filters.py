46d262c6c0b27beb080315697eb1412a034256b7
noam gavish <gavishnoam@gmail.com>
2014-10-10 23:12:10 +0300

    filters to not import ContinuousDataEven

--- signal_processing/continuous/filters.py
+++ signal_processing/continuous/filters.py
@@ -2,7 +2,6 @@ import warnings
 
 import scipy as sp
 
-from continuous_data_even_obj import ContinuousDataEven
 
 from signal_processing.extensions import pint_extension
 from signal_processing.extensions import scipy_extension
@@ -29,5 +28,5 @@ def band_pass_filter(sig, freq_range, mask_len):
     # if error rises with firwin with units, wrap it: http://pint.readthedocs.org/en/0.5.1/wrapping.html
     mask_1 = sp.signal.firwin(mask_len, freq_range.edges.magnitude, pass_zero=False, nyq=0.5 * sig.sample_rate.magnitude)
     filterred_values = scipy_extension.smart_convolve(sig.values.magnitude, mask_1, mode="same") * pint_extension.get_units(sig.values)
-    filterred = ContinuousDataEven(filterred_values, sig.sample_step, sig.first_sample)
+    filterred = sig.new_values(filterred_values)
     return filterred
