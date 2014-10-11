afa2f411e37b526557346bca5046ef78cd5bf651
noam gavish <gavishnoam@gmail.com>
2014-10-11 20:59:25 +0300

    fixed other examples

--- code_examples/band_pass_filter.py
+++ code_examples/band_pass_filter.py
@@ -1,14 +1,16 @@
 from signal_processing import Segment
+from signal_processing import ContinuousDataEven
 from signal_processing import continuous as cont
 from signal_processing import U_
 
-white_noise = cont.generators.white_noise(U_.sec, n_samples=2**16, amplitude=U_.mamp)
+white_noise = ContinuousDataEven.generate('white_noise', U_.sec,
+                                          n_samples=2**16, amplitude=U_.mamp)
 white_noise.values_description = "white noise"
 
 freq_range = Segment([0.3, 0.4], U_.Hz)
-white_noise_filterred = cont.filters.band_pass_filter(white_noise, freq_range, mask_len=32)
+white_noise_filterred = cont.filters.band_pass_filter(white_noise, freq_range,
+                                                      mask_len=32)
 white_noise_filterred.values_description = "filterred"
 
 cont.plot_few(white_noise, white_noise_filterred)
 cont.plot_few(white_noise.fft().abs(), white_noise_filterred.fft().abs())
-
--- code_examples/sine.py
+++ code_examples/sine.py
@@ -1,12 +1,17 @@
 from signal_processing import Segment
+from signal_processing import ContinuousDataEven
 from signal_processing import continuous as cont
 from signal_processing import U_
 
-sig_line = cont.generators.generate_sine(U_.msec, 2 ** 7, 270 * U_.volt, sine_freq=50 * U_.Hz) + \
-cont.generators.white_noise(U_.msec, 2 ** 7, 270 * 0.6 * U_.volt)
-freq_range = Segment([48.0, 52.0], U_.Hz)
+sig_line_0 = ContinuousDataEven.generate('sine', U_.msec, 2 ** 7,
+                                    270 * U_.volt, sine_freq=50 * U_.Hz)
+noise = ContinuousDataEven.generate('white_noise', U_.msec, 2 ** 7,
+                                    270 * 0.6 * U_.volt)
+sig_line = sig_line_0 + noise
 
-sig_line_clean = cont.filters.band_pass_filter(sig_line, freq_range, mask_len=32)
+freq_range = Segment([48.0, 52.0], U_.Hz)
+sig_line_clean = cont.filters.band_pass_filter(sig_line, freq_range,
+                                               mask_len=32)
 
 sig_line.values_description = 'sine unclean'
 sig_line_clean.values_description = 'clean'
