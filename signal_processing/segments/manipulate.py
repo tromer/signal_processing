09eaf566b18af8424247e41cbf9f8648c1df9f5c
noam gavish <gavishnoam@gmail.com>
2014-10-19 01:05:06 +0300

    some pep8 style fixes, new parameter for concatenate of segments, allow concatenation of segments not in order

--- signal_processing/segments/manipulate.py
+++ signal_processing/segments/manipulate.py
@@ -8,26 +8,28 @@ from segments_obj import Segments
 
 import adjoin
 
-def concatenate(segments_list):
+def concatenate(segments_list, XXX_allow_not_in_order=False):
     """
     concatenates segments, if they are all one after another
     TOOD: test when there are empty segments
     """
     # filter out empty segments instances
     segments_list = filter(None, segments_list)
-    
-    for i in xrange(len(segments_list) - 1):
-        if segments_list[i].ends[-1] > segments_list[i + 1].starts[0]:
-            raise ValueError("not in order")
-    
 
-        
+    if not XXX_allow_not_in_order:
+        for i in xrange(len(segments_list) - 1):
+            if segments_list[i].ends[-1] > segments_list[i + 1].starts[0]:
+                raise ValueError("not in order")
+
+    # this is a thin patch for cases where we don't enforce segments in order
+    else:
+        pass
+
+
     all_starts = map(lambda segments : segments.starts, segments_list)
     all_ends = map(lambda segments : segments.ends, segments_list)
     return Segments(pint_extension.concatenate(all_starts), pint_extension.concatenate(all_ends))
 
-
-   
 
 def concatenate_single_segments(segs_list):
     """
@@ -43,7 +45,7 @@ def concatenate_single_segments(segs_list):
     other option, which may work - start with sorting by start, and also by end of the segment. maybe it will say something smart, and thus avoid the loop
     """
     #as_segments = map(Segments.from_single_segment, segs_list)
-    #return concatenate(as_segments)    
+    #return concatenate(as_segments)
 
     raise NotImplementedError
     sorted_by_start = sorted(segments_list, key=lambda s : s.start)
@@ -52,27 +54,27 @@ def concatenate_single_segments(segs_list):
     segments_maybe_overlap = Segments(starts, ends)
     segments = adjoin.max_dist(segments_maybe_overlap, max_distance=0 * pint_extension.get_units(segments_maybe_overlap.starts))
     return segments
- 
+
 
 def switch_segments_and_gaps(segments, absolute_start=None, absolute_end=None):
     """
     returns the gaps between the segments as a Segments instance
     rational: sometimes it's easier to understand what are the segments which dosn't
     interest us, and then switch
-    
+
     parameters
     --------------
     segs : Segments
-    
+
     absolute_start, absolute_end : of the same unit like segments.starts
         if given, they represent the edges of the "signal", and thus
         create another "gap-segment" at the start / end.
-        
+
     returns
     ---------------
     gaps: Segments
         the gaps
-    
+
     """
     # maybe absolute start and end should be taken from the segments object?
     starts_gaps = segments.ends[:-1]
@@ -83,7 +85,7 @@ def switch_segments_and_gaps(segments, absolute_start=None, absolute_end=None):
     if absolute_end:
         raise NotImplementedError
         ends_gaps = np.concatenate([ends_gaps, np.ones(1) * absolute_end])
-    
+
     return Segments(starts_gaps, ends_gaps)
 
 
@@ -92,4 +94,4 @@ def filter_short_segments(segments, min_duration):
     TODO: it should be based on filter_by_range
     """
     return segments.filter_by_range('durations', Segment([0, min_duration]), mode='remove')
- 
+
