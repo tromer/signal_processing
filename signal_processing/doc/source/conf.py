6d410d934c0c35d4f2307ca459b9405ac257f58f
noam gavish <gavishnoam@gmail.com>
2014-10-21 16:10:32 +0300

    using numpydoc extension to sphinx

--- signal_processing/doc/source/conf.py
+++ signal_processing/doc/source/conf.py
@@ -30,6 +30,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'numpydoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
