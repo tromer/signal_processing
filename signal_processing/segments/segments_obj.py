created a nicer interface for __init__ of Segments objects. now you can specify the units for starts and ends together. corrected the tests (and also found and corrected mistajes in the tests)

signal_processing/segments/segments_obj.py
@@ -65,7 +65,7 @@ class Segments(object):
 
     TODO: add interface of starts, ends, unit=None
     """
-    def __init__(self, starts, ends):
+    def __init__(self, starts, ends, unit=None):
         """
         parameters:
         ---------------------
@@ -81,6 +81,20 @@ class Segments(object):
             warnings.warn("the segments are not strictly one after another")
         """
         assert len(starts) == len(ends)
+
+        if hasattr(starts, 'units') and hasattr(ends, 'units'):
+            if starts.dimensionality != ends.dimensionality:
+                raise ValueError(
+                    'starts and ends with diffrent dimensionalities')
+
+        elif unit != None:
+            if hasattr(starts, 'units') or hasattr(ends, 'units'):
+                raise ValueError('only starts / ends has units')
+            starts = np.array(starts) * unit
+            ends = np.array(ends) * unit
+
+        else:
+            raise ValueError("units not given")
 
         self._starts = starts
         self._ends = ends

signal_processing/tests/test_segments/test_manipulate.py
@@ -11,8 +11,8 @@ from signal_processing import U_
 def test_switch_segments_and_gaps():
     starts = np.array([0, 2, 4, 10])
     ends = np.array([1, 3, 5, 10.5])
-    segments_0 = Segments(starts, ends)
-    expected_gaps = Segments(np.array([1, 3, 5]), np.array([2, 4, 10]))
+    segments_0 = Segments(starts, ends, U_.dimensionless)
+    expected_gaps = Segments([1, 3, 5], [2, 4, 10], U_.dimensionless)
     gaps = manipulate.switch_segments_and_gaps(segments_0)
     assert gaps.is_close(expected_gaps)
     """

signal_processing/tests/test_segments/test_segments_obj.py
@@ -27,6 +27,10 @@ def test_segments():
     # __getitem__
     assert np.allclose(starts[is_each_in], p[is_each_in].starts)
     assert np.allclose(starts[s], p[s].starts)
+
+    # other ways to create segments
+    p_2 = Segments([0, 2, 4, 10], [1, 3, 5, 10.5], unit=U_.sec)
+    assert p.is_close(p_2)
     """
     print p
     print p.starts
@@ -105,7 +109,7 @@ def test_to_segments_list():
 
 def test_from_single_segment():
     s = Segment([2, 3], U_.meter)
-    expected_segments = Segments(np.array([2,]) * U_.meter, np.array([3,]))
+    expected_segments = Segments([2,], [3,], U_.meter)
     segments = Segments.from_single_segment(s)
     assert segments.is_close(expected_segments)
 

signal_processing/tests/test_threshold.py
@@ -12,7 +12,7 @@ def test_crosses():
     threshold_0 = 2 * U_.mamp
     starts_expected = np.array([0, 6])
     ends_expected = np.array([3, 8])
-    segments_expected = Segments(starts_expected, ends_expected)
+    segments_expected = Segments(starts_expected, ends_expected, U_.sec)
     segments = threshold.crosses(sig, threshold_0)
     assert segments.is_close(segments_expected)
 